ea786b75226ba5c41b07ab656c204f432068e946
Reid Watson <j.watsonreid@gmail.com>
2020-09-04 20:16:45 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -52,10 +52,10 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-    programmer_hash[:dennis_ritchie][:languages].flatten
+    programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
